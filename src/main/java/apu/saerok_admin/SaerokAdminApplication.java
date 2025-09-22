fe4470292104a95fd9350ed2fdf56ba608af9d9c
soonduck-dreams <nederileni99heits@gmail.com>
2025-09-22 10:18:36 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/apu/saerok_admin/SaerokAdminApplication.java
@@ -0,0 +1,13 @@
+package apu.saerok_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaerokAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaerokAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apu/saerok_admin/SaerokAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package apu.saerok_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaerokAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
