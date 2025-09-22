11c4cc75adabfe68f1ea1d6b3de98dc74e0d55e9
Junhee Han <115782193+soonduck-dreams@users.noreply.github.com>
2025-09-22 14:49:46 +0900

    Provide empty user store to disable default password (#5)

---
+++ src/main/java/apu/saerok_admin/web/HomeController.java
@@ -0,0 +1,13 @@
+package apu.saerok_admin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
